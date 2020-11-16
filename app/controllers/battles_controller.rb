b469672302d861a2225e07a24a74713b221bf2e4
Julien Bouland <bouland.julien@gmail.com>
2020-11-16 19:09:53 +0100

    Score logic battle controller

--- app/controllers/battles_controller.rb
+++ app/controllers/battles_controller.rb
@@ -7,29 +7,10 @@ class BattlesController < ApplicationController
   end
   
   def show
-    @player_1 = Player.find(@battle.player_1_id)
-    @player_2 = Player.find(@battle.player_2_id)
-    @score_1 = score(@player_1)
-    @score_2 = score(@player_2)
-    if(@score_1 == @score_2)
-      @winner = nil
-    else
-      if @score_1 > @score_2
-        @winner = @player_1
-        @player_2.life_points -= 0.5
-        @player_2.save
-        @player_1.life_points += 0.5
-        @player_1.attack_points += 0.8
-        @player_1.save
-      else
-        @winner = @player_2
-        @player_1.life_points -= 0.5
-        @player_1.save
-        @player_2.life_points += 0.5
-        @player_2.attack_points += 0.8
-        @player_2.save
-      end
-    end
+    @winner = @battle.winner unless @battle.draw
+    @loser = @battle.loser unless @battle.draw
+    @winner_score = @battle.winner_score unless @battle.draw
+    @loser_score = @battle.loser_score unless @battle.draw
   end
 
   def new
@@ -39,24 +20,32 @@ class BattlesController < ApplicationController
   end
 
   def create
-    @battle = Battle.create(battle_params)
+    @battle = Battle.new(battle_params)
     if @battle.save
+      Battle.update_battle_scores(@battle)
+      if @battle.score1 == @battle.score2
+        @battle.draw = true
+        @battle.save
+      else
+        Battle.update_battle_winner_loser(@battle)
+        Player.adjust_life_attack(@battle)
+      end
       redirect_to battle_path(@battle)
     else
+      flash.now[:alert] = flash_alerts
       render :new
     end
   end
 
   private
 
-  def score(player)
-    attack = player.attack_points
-    strength = player.strength_points
-    intelligence = player.intelligence_points
-    magic = player.magic_points
-    attack + strength * 0.8 + intelligence * 0.7 + magic * 0.9
+  def flash_alerts
+    errors = @battle.errors.full_messages
+    not_ok = "Player 2 can't be blank"
+    ok = 'Please select two players'
+    errors.map { |m| m.gsub(not_ok, ok) }.join(' - ')
   end
-
+  
   def set_battle
     @battle = Battle.find(params[:id])
   end
